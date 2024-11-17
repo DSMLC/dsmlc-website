75059d221bb1d264e6073a75e082c9a6b6de5649
Jason Nguyen <Jason_D_N@hotmail.com>
2024-11-17 13:01:17 -0700

    small update to remove unused code

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,7 @@
 import rawData from "../public/data/page_data/home.json";
 import DataLoader, { PageData } from "./DataLoader";
-import CarouselTemplate from "./components/templates/CarouselTemplate";
 
 const HomeData: PageData[] = rawData as PageData[];
-
-const images = [
-  { src: "/images/events/fall_2024/int_and_app.png", alt: "Description 1" },
-  { src: "/images/events/fall_2024/int_and_app2.png", alt: "Description 2" },
-  {
-    src: "/images/events/fall_2024/meet_and_greet.png",
-    alt: "Description 3",
-  },
-];
 
 export default async function Home() {
   return (
