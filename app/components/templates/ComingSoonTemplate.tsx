0befb7d81021d3581a0c93bdb30146ad6fbada5f
LychellePham <lychellepham731@gmail.com>
2025-07-01 13:50:40 -0600

    fix code in dataLoader and template

--- app/components/templates/ComingSoonTemplate.tsx
+++ app/components/templates/ComingSoonTemplate.tsx
@@ -8,17 +8,10 @@ const TitleTemplate2 = ({ Data }: { Data: ComingSoonTemplateData["data"] }) => {
     <>
     <div className="border-2 border-dashed border-dsmlcTangerine rounded-md p-3">
       <h1 className="lg:text-3xl md:text-2xl text-xl font-extrabold text-center text-dsmlcTangerine mb-5 font-redHat leading-tight pt-3 pb-3">
-<<<<<<< HEAD
         {Data.header}
       </h1>
       <p className="lg:text-lg md:text-md text-base text-center dark:text-dark-dsmlcBlack text-light-dsmlcBlack mb-2 font-quicksand">
         {Data.subtext}
-=======
-        Check Back for Future Events!
-      </h1>
-      <p className="lg:text-lg md:text-md text-base text-center dark:text-dark-dsmlcBlack text-light-dsmlcBlack mb-2 font-quicksand">
-        Keep up with our socials for updates!
->>>>>>> b7f95da0f680f58a2d6b6bb95d53e657b44278a6
       </p>
       <SocialLinksTemplate Data={{}}></SocialLinksTemplate>
 
