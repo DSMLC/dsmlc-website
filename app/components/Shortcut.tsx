904e87b2b22e6a8bc1c316800c3f4533048f6286
LychellePham <lychellepham731@gmail.com>
2025-08-03 22:22:13 -0600

    change colour based on light/dark mode, and have the Shortcut component only appear on large screens(hide it for smaller screens)

--- app/components/Shortcut.tsx
+++ app/components/Shortcut.tsx
@@ -1,6 +1,10 @@
 "use client";
 import { usePathname } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import { useTheme } from "../ThemeProvider";
+
+
+
 
 type Anchor = {id: string; label: string};
 
@@ -8,6 +12,7 @@ type Anchor = {id: string; label: string};
 
 export default function Shortcut({anchors}: {anchors: Anchor[]}) {
 
+  const {isDarkMode} = useTheme();
 
   const handleClick = (id: string) => {
     const target = document.getElementById(id);
@@ -17,8 +22,9 @@ export default function Shortcut({anchors}: {anchors: Anchor[]}) {
   };
 
 
+  const sideBarBaseStyle = "fixed top-0 right-0 h-full w-64 border-solid p-5 pt-32 border-l-4 border-dsmlcTangerine transform translate-x-full xl:translate-x-0 transition-transform duration-300 ease-in-out z-10";
   return (
-    <div id="sidebar" className="fixed top-0  right-0 h-full w-64 bg-dark-dsmlcWhite/75 text-white border-solid p-5 pt-32 border-l-4 border-dsmlcTangerine transform translate-x-full md:translate-x-0 transition-transform duration-300 ease-in-out z-10">
+    <div id="sidebar" className={isDarkMode ? `${sideBarBaseStyle} bg-dark-dsmlcWhite/75 text-white` : `${sideBarBaseStyle} bg-light-dsmlcParchment/75 text-dark-dsmlcParchment`}>
     <h2 className="text-2xl font-bold mb-6">ShortCut</h2>
     <ul className="space-y-4">
       {anchors.map((anchor) => (
