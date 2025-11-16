7e27e11d1c1f545929761dfe7144a799f4ae1382
Joseph Tandyo <josephtandyo@gmail.com>
2025-11-15 19:01:52 -0700

    Feature: Changed coming soon template to be dynamic

--- app/components/templates/ComingSoonTemplate.tsx
+++ app/components/templates/ComingSoonTemplate.tsx
@@ -1,23 +1,45 @@
 "use client";
-import { ComingSoonTemplateData } from "@/app/DataLoader";
-import React from "react";
+import {
+  ComingSoonTemplateData,
+  PageData,
+  resolveData,
+} from "@/app/DataLoader";
+import React, { useEffect, useState } from "react";
 import SocialLinksTemplate from "./SocialLinksTemplate";
 
-const ComingSoonTemplate = ({ Data }: { Data: ComingSoonTemplateData["data"] }) => {
+const ComingSoonTemplate = ({
+  Data,
+}: {
+  Data: ComingSoonTemplateData["data"];
+}) => {
+  const [resolvedConditionData, setResolvedConditionData] = useState<
+    any | null
+  >(null);
+
+  useEffect(() => {
+    (async () => {
+      const data = await resolveData(Data.condition as PageData);
+      setResolvedConditionData(data);
+    })();
+  }, [Data.condition]);
+
+  if (Data.condition.type === "array") {
+    if (resolvedConditionData != null && resolvedConditionData.length > 0) {
+      return;
+    }
+  }
+
   return (
     <>
-    <div className="border-2 border-dashed border-dsmlcTangerine rounded-md p-3">
-      <h1 className="lg:text-3xl md:text-2xl text-xl font-extrabold text-center text-dsmlcTangerine mb-5 font-redHat leading-tight pt-3 pb-3">
-        {Data.header}
-      </h1>
-      <p className="lg:text-lg md:text-md text-base text-center dark:text-dark-dsmlcBlack text-light-dsmlcBlack mb-2 font-quicksand">
-        {Data.subtext}
-      </p>
-      <SocialLinksTemplate Data={{}}></SocialLinksTemplate>
-
-
-    </div>  
-      
+      <div className="border-2 border-dashed border-dsmlcTangerine rounded-md p-3">
+        <h1 className="lg:text-3xl md:text-2xl text-xl font-extrabold text-center text-dsmlcTangerine mb-5 font-redHat leading-tight pt-3 pb-3">
+          {Data.header}
+        </h1>
+        <p className="lg:text-lg md:text-md text-base text-center dark:text-dark-dsmlcBlack text-light-dsmlcBlack mb-2 font-quicksand">
+          {Data.subtext}
+        </p>
+        <SocialLinksTemplate Data={{}}></SocialLinksTemplate>
+      </div>
     </>
   );
 };
